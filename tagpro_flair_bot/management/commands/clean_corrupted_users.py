7e19e3b7edf4d1211d5fded2c2f80f76da5fda0a
Precious Roy <TagPro-PreciousRoy@users.noreply.github.com>
2015-10-29 15:31:12 -0400

    Use the custom user for the cleanup command

--- tagpro_flair_bot/management/commands/clean_corrupted_users.py
+++ tagpro_flair_bot/management/commands/clean_corrupted_users.py
@@ -1,5 +1,5 @@
 from django.core.management.base import NoArgsCommand
-from django.contrib.auth.models import User
+from tagpro_flair_bot.models import TagProUser as User
 
 
 class Command(NoArgsCommand):
